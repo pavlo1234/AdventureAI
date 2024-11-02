445cdb9042c7d87c8bd6f60516d4c4c1f98f4732
pavlo_klochynskyi <klochinsky12pavlo@gmail.com>
2024-11-02 18:56:03 +0200

    Init Django project. Implement sign-in and sign-up endpoints

---
+++ server/project/project/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class CredentialsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['email'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        
+        return user
